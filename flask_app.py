03565c78079395fe9567b697c9c5831685a15d89
xiurobert <xiurobert@gmail.com>
2017-04-06 12:46:57 +0000

    Update FibFiles

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,45 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/')
-def hello_world():
-    return render_template("fibtor.html")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("fibtor.html", comments=Comment.query.all())
+
+    comment = Comment(content=request.form["contents"])
+    fibtor.session.add(comment)
+    fibtor.session.commit()
+    return redirect(url_for('index'))
 
 
+# Connect to the Fibtor service
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="xiurobert",
+    password="fibtorisgay",
+    hostname="xiurobert.mysql.pythonanywhere-services.com",
+    databasename="xiurobert$fibtor_gay",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+
+
+fibtor = SQLAlchemy(app)
+
+class Comment(fibtor.Model):
+
+    __tablename__ = "fibtor"
+
+    id = fibtor.Column(fibtor.Integer, primary_key=True)
+    content = fibtor.Column(fibtor.String(4096))
 
